2f039b55a0b4b1c55ca2231d2105cff36e45c95e
Sascha G <sushi.genji@hotmail.de>
2012-02-14 20:12:13 +0100

    0.9Version rdy to run

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Sascha::Application.routes.draw do
   root :to => 'pages#home'
-  match "3dwork" => 'pages#3dwork'
+  match "Bilder" => 'pages#3dwork'
+  match "Autor" => 'pages#Autor'
+  match "uebermich" => 'pages#uebermich'
+  match "projekte" => 'pages#projekte'
+  match "sonstiges" => 'pages#sonstiges'
+  match "Impressum" => 'pages#Impressum'
+  
+  match "Projekte/esisttot" => 'pages#projekte_esisttot'
+  match "Projekte/Splatterkram" => 'pages#projekte_Splatterkram'
+  match "Autor/WeltendesNebels" => 'pages#Autor_WeltendesNebels'
 
   
 end
